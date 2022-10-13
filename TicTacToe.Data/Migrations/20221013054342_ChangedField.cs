13812eea15d2aaf5262eaa6b50406e4ca971cae0
Mayank Agrawal <mayank@caizin.com>
2022-10-13 11:17:13 +0530

    updated database to add foreign key

---
+++ TicTacToe.Data/Migrations/20221013054342_ChangedField.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicTacToe.Data.Migrations
+{
+    public partial class ChangedField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Players_PlayerId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_PlayerId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "PlayerId",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "XPlayerId",
+                table: "Games",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_XPlayerId",
+                table: "Games",
+                column: "XPlayerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Players_XPlayerId",
+                table: "Games",
+                column: "XPlayerId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Players_XPlayerId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_XPlayerId",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "XPlayerId",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlayerId",
+                table: "Games",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_PlayerId",
+                table: "Games",
+                column: "PlayerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Players_PlayerId",
+                table: "Games",
+                column: "PlayerId",
+                principalTable: "Players",
+                principalColumn: "Id");
+        }
+    }
+}
